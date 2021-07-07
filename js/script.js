26628969e8cf4fa782c6760ef32e908483375fca
Polina <991201@bk.ru>
2021-07-07 19:41:52 +0300

    create countdown timer on the site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    // Табы на странице
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,4 +35,52 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Таймер
+    let deadline = '2021-07-08';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            
+            function addZeroForNumber(number) {
+                if (number < 10) {
+                    return '0' + number;
+                } else return number;
+            }
+            
+            hours.textContent = addZeroForNumber(t.hours);
+            minutes.textContent = addZeroForNumber(t.minutes);
+            seconds.textContent = addZeroForNumber(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
