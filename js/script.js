d421ed6adac762d2c8d3b06ca6ad9c2cb1c5a4ac
Polina <991201@bk.ru>
2021-07-08 12:41:06 +0300

    create a modal window

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     // Таймер
-    let deadline = '2021-07-08';
+    let deadline = '2021-07-09';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -83,4 +83,30 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    // Модальное окно
+    let more = document.querySelector('.more'),
+        overvlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        description = document.querySelectorAll('.description-btn');
+
+    function showModal(btn) {
+        btn.addEventListener('click', function() {
+            overvlay.style.display = 'block';
+            this.classList.add('more-splash'); 
+            document.body.style.overflow = 'hidden';
+        });
+    
+        close.addEventListener('click', function() {
+            overvlay.style.display = 'none';
+            btn.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
+    }
+
+    showModal(more);
+    
+    description.forEach(function(item) {
+        showModal(item);
+    });
 });
