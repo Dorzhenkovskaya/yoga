614f0ebb07b3c07a6f1e0310b9a23dbfdc46bd5d
Polina <991201@bk.ru>
2021-07-12 16:37:06 +0300

    create a script for submitting data from a form

--- js/script.js
+++ js/script.js
@@ -109,4 +109,60 @@ window.addEventListener('DOMContentLoaded', function() {
     description.forEach(function(item) {
         showModal(item);
     });
+
+    // Форма
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        formContact = document.getElementById('form'),
+        inputContact = formContact.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    function submitForm(nameForm, inputForm) {
+        nameForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            nameForm.appendChild(statusMessage);
+    
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    
+            // let formData = new FormData(nameForm);
+            // request.send(formData);
+
+            let formData = new FormData(nameForm);
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+    
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if(request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+    
+            for (let i = 0; i < inputForm.length; i++) {
+                inputForm[i].value = '';
+            }
+        });
+    }
+
+    submitForm(form, input);
+    submitForm(formContact, inputContact);
 });
